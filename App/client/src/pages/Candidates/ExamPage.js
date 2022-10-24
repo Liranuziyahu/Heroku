46399fe5d5dce3b3070d11d431f47f6b311ae0d5
Liranuziyahu <liranuzistud@gmail.com>
2022-10-24 22:34:59 +0300

    better change files

---
+++ App/client/src/pages/Candidates/ExamPage.js
@@ -0,0 +1,69 @@
+import React,{useEffect , useContext , useState} from 'react'
+import { useParams , useLocation } from "react-router-dom";
+import {ContextFromServer} from '../../context'
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+const ExamPage = () => {
+  const params = useParams();
+  const {AnswersByExamID ,exams} = useContext(ContextFromServer)
+  const [exam, setExam] = useState()
+  const [user , setUser] = useState()
+  const location = useLocation()
+  const { data } = location?.state
+
+  const score = {
+    float: 'right',
+    marginRight:50,
+    fontSize:70,
+    width:200,
+    height:200,
+    color:'red',
+    border:'3px solid red',
+    borderRadius:'50%',
+    textAlign:'center',
+    textJustify:'center',
+    transform: 'rotate(0.96turn)',
+    padding:'40px',
+  }
+  const reload = {
+   position: 'absolute',
+   top:'calc(50% - 32px)',
+   left:'calc(50% - 32px)'
+  }
+
+  useEffect(async ()=>{
+   let answers = await AnswersByExamID(params.id)
+   setExam(answers)
+  },[exams])
+  return (
+    <>
+
+      {
+     exam == undefined 
+     ? 
+      <Box style={reload} sx={{ display: 'flex' }}>
+        <CircularProgress size="4rem"/>
+     </Box> 
+    :
+    exam?.message ? 
+    <div> {exam?.message} </div>
+      :
+     [
+        <div style={score}>{data.score}</div>,
+
+        exam?.map(answer => (
+        <div key={answer.questionID} style={{marginBottom:20}}>
+          <div>{answer.questionTheQuestion}</div>
+          <div style={answer.questionTrueAnswer == answer.answer1 ? {color:'green'}: (answer.answer1 == answer.userAnswer && answer.userAnswer != answer.questionTrueAnswer )?{color:'red'}:null}>1. {answer.answer1}</div>
+          <div style={answer.questionTrueAnswer == answer.answer2 ? {color:'green'}: (answer.answer2 == answer.userAnswer && answer.userAnswer != answer.questionTrueAnswer )?{color:'red'}:null}>2. {answer.answer2}</div>
+          <div style={answer.questionTrueAnswer == answer.answer3 ? {color:'green'}: (answer.answer3 == answer.userAnswer && answer.userAnswer != answer.questionTrueAnswer )?{color:'red'}:null}>3. {answer.answer3}</div>
+          <div style={answer.questionTrueAnswer == answer.answer4 ? {color:'green'}: (answer.answer4 == answer.userAnswer && answer.userAnswer != answer.questionTrueAnswer )?{color:'red'}:null}>4. {answer.answer4}</div>
+        </div>))  
+      ]    
+      }
+    </>
+  )
+}
+
+export default ExamPage
---
+++ App/client/src/pages/Candidates/TabelCandidates.js
@@ -0,0 +1,72 @@
+import React,{useContext, useState} from 'react'
+//Component
+import {ContextFromServer} from '../../context'
+import Candidates from './Candidates'
+//MUI
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import InputSearch from '../Buttons/InputSearch';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+
+
+const TabelCandidates = () => {
+    const {exams} = useContext(ContextFromServer)
+    const [userSearch ,setUserSearch] = useState({search:"" , catagorey:""})
+    const [page, setPage] = useState(1);
+
+    // exams?.sort?.((exam1 , exam2)=>{
+    //   return exam1.userID - exam2.userID
+    // })
+
+    const handleChange = (event, value) => {
+      setPage(value);
+    };
+
+
+
+    return (
+      <div style={{overflowX: 'hidden'}}>
+        <InputSearch setUserSearch = {setUserSearch}></InputSearch>
+        <TableContainer component={Paper} style={{overflowY: 'scroll',width: '100%'}}>  
+          <Table sx={{ minWidth: 200 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+              <TableCell align="center">User ID</TableCell>
+                <TableCell align="center">User Name</TableCell>
+                <TableCell align="center">Email</TableCell>
+                <TableCell align="center">Type</TableCell>
+                <TableCell align="center">Score</TableCell>
+                <TableCell align="center">Exam</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {
+                userSearch.search?
+                  (
+                    exams?.map?.((exam ,index)=>{
+                    let entryCategory = eval(`exam.${userSearch?.catagorey}`)?.toString()?.toLowerCase()
+                    if(entryCategory?.includes?.(userSearch.search?.toString()?.toLowerCase()))
+                        return <Candidates key={index} exam = {exam}/>
+                    })
+                  ): exams?.length != undefined ?
+                  exams?.slice(5 * page - 5, 5 * page)?.map?.((exam,index) => <Candidates  key = {index} exam = {exam}/>) : null
+                }
+            </TableBody>
+          </Table>
+      </TableContainer>
+            
+      <Stack direction="row" spacing={2} sx={{marginTop:2 , justifyContent: 'center'}}>
+            <Pagination count={exams?.length > 0 ? exams?.length /5 % 1 == 0 ? exams?.length / 5 : Number.parseInt(exams?.length / 5) + 1 : 0}
+              onChange={handleChange}
+            />
+      </Stack>
+    </div>
+  )
+}
+export default TabelCandidates
