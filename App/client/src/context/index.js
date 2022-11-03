68eb786cc4d28b4ad9a0e0239f8bcec1fff47e0e
Liranuziyahu <liranuzistud@gmail.com>
2022-11-03 15:59:08 +0200

    change path API

--- App/client/src/context/index.js
+++ App/client/src/context/index.js
@@ -15,12 +15,13 @@ const ContextServer = ({children}) => {
   const [questionsAngular , setQuestionsAngular] = useState([])
   const [categoryExam , setCategoryExam] = useState([])
   const [updateContext , setUpdateContext] = useState(false)
+  const API = 'https://queezer.herokuapp.com/'
   const navigate = useNavigate();
 
   useEffect(async () =>{
-    await axios.get(`http://localhost:8080/users`).then(data => setAllUsers(data?.data))
-    await axios.get(`http://localhost:8080/exams`).then(data => setExams(data?.data))
-    await axios.get(`http://localhost:8080/questions`)
+    await axios.get(API+`users`).then(data => setAllUsers(data?.data))
+    await axios.get(API+`exams`).then(data => setExams(data?.data))
+    await axios.get(API+`questions`)
     .then(data => {
       setQuestionsJS(data?.data.filter(question => question.categoryExamsName == 'JS'))
       setQuestionsReact(data?.data.filter(question => question.categoryExamsName == 'React'))
@@ -37,20 +38,20 @@ const ContextServer = ({children}) => {
         question['categoryExamsID'] = '2'
     if(category=='Angular')
         question['categoryExamsID'] = '3'
-    axios.post('http://localhost:8080/questions',question)
+    axios.post(API+'questions',question)
 }
 
 /////////User/////////
   //Create
   const createUser = (async (user , categorys)=>{
 
-    await axios.post('http://localhost:8080/users',user)
+    await axios.post(API+'users',user)
     .then(res => {
       if(res.data.message!= undefined) 
         alert(res.data.message)
       else
       {
-        axios.post('http://localhost:8080/exams',{userID:res.data.userID, categorys:categorys})
+        axios.post(API+'exams',{userID:res.data.userID, categorys:categorys})
         setUser(userModle)
         setCategoryExam([])
         setUpdateContext(!updateContext)
@@ -60,19 +61,19 @@ const ContextServer = ({children}) => {
     .catch(err => err)
   })
   //Edit
-  const editUser = (user => axios.put(`http://localhost:8080/users/${user.userID}`,user))
+  const editUser = (user => axios.put(API+`users/${user.userID}`,user))
   //Delete 
-  const deleteUser = (async userID => await axios.delete(`http://localhost:8080/users/${userID}`))
+  const deleteUser = (async userID => await axios.delete(API+`users/${userID}`))
 
 /////////Exam/////////
   //Create
-  const createExam = (async exam => await axios.post('http://localhost:8080/exams/',exam))
+  const createExam = (async exam => await axios.post(API+'exams/',exam))
   //Update specific exam - for change score
-  const editExam = (async exam => await axios.put(`http://localhost:8080/exams/${exam.examsID}`,exam))
+  const editExam = (async exam => await axios.put(API+`exams/${exam.examsID}`,exam))
   //Delete All Exams 
   const DeleteAllExams = (async userID => {
     await DeleteAnswersByID(userID)  
-    await axios.delete(`http://localhost:8080/exams/deleteAll/${userID}`)
+    await axios.delete(API+`exams/deleteAll/${userID}`)
   })
   //Update Exams User ask to test
   const updateExams = ( async (exam , categorys) => {
@@ -88,15 +89,15 @@ const ContextServer = ({children}) => {
 /////////Answers Exam/////////
   //Create
   const CreateAnswersExam = (answers =>{
-    answers.map( answer => axios.post('http://localhost:8080/answer',answer))
+    answers.map( answer => axios.post(API+'answer',answer))
   })
   //Detele
   const DeleteAnswersByID = ( async id =>{
-    await axios.delete(`http://localhost:8080/answer/${id}`)
+    await axios.delete(API+`answer/${id}`)
   })
   //Get by ID
   const AnswersByExamID = (async examID =>{
-    return  axios.get(`http://localhost:8080/answer/${examID}`)
+    return  axios.get(API+`answer/${examID}`)
     .then(res=>  res.data)
   })
 
@@ -116,7 +117,7 @@ const ContextServer = ({children}) => {
 
   return (
     <div>
-        <ContextFromServer.Provider value={{user , setUser ,createUser,deleteUser,allUsers,setAllUsers ,setUpdateContext,updateContext,exams , questionsJS , questionsReact , questionsAngular ,editUser ,createQuestion ,editExam , createExam , updateExams , checkCategory , categoryExam ,CreateAnswersExam,DeleteAllExams,DeleteAnswersByID,AnswersByExamID}}>
+        <ContextFromServer.Provider value={{API ,user , setUser ,createUser,deleteUser,allUsers,setAllUsers ,setUpdateContext,updateContext,exams , questionsJS , questionsReact , questionsAngular ,editUser ,createQuestion ,editExam , createExam , updateExams , checkCategory , categoryExam ,CreateAnswersExam,DeleteAllExams,DeleteAnswersByID,AnswersByExamID}}>
           {children}
         </ContextFromServer.Provider>
     </div>
